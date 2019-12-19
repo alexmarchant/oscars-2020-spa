b9e0cfc014e924c65864bdc7a3e935426e8830c5
Alex Marchant <alexjmarchant@gmail.com>
2019-12-18 21:14:12 -0600

    Some more boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Container>
       <h1>Oscars 2020 SPA</h1>
-    </div>
+    </Container>
   );
 }
 
