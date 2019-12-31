b55accd5b1eebabd5fd91a3353ef5747af6984c4
Alex Marchant <alexjmarchant@gmail.com>
2019-12-30 22:11:56 -0600

    Add some apollo stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
 import styled from 'styled-components'
+import gql from 'graphql-tag'
 
-const Container = styled.div`
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Code = styled.pre`
+  border: 1px solid #333;
+  background-color: #ddd;
+  padding: 10px;
+`
+
+const getEverythingQueryString = `query GetEverything {
+  testPayload {
+    string
+    int
+    float
+    bool
+  }
+  counter
+}`
+const GET_EVERYTHING = gql`
+  query GetEverything {
+    testPayload {
+      string
+      int
+      float
+      bool
+    }
+    counter
+  }
 `
 
 const App: React.FC = () => {
+  const { data, loading, error } = useQuery(GET_EVERYTHING)
+  if (loading) return <p>LOADING</p>
+  if (error) return <p>ERROR</p>
+
+  const dataString = JSON.stringify(data, null, 2)
+
   return (
-    <Container>
-      <h1>Oscars 2020 SPA</h1>
-    </Container>
+    <div>
+      <strong>Query:</strong>
+      <Code>{getEverythingQueryString}</Code>
+      <strong>Result:</strong>
+      <Code>{dataString}</Code>
+      <hr></hr>
+      <strong>Mutation:</strong>
+      <Code></Code>
+      <strong>Trigger Mutation:</strong>
+      <br></br>
+      <br></br>
+      <button>Increment</button>
+      <br></br>
+      <br></br>
+      <strong>Result</strong>
+      <Code>{data.counter}</Code>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
+import { ApolloProvider } from '@apollo/react-hooks'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const cache = new InMemoryCache()
+const link = new HttpLink({ uri: 'http://localhost:3001/' })
+const client = new ApolloClient({
+  cache,
+  link,
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
