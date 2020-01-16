77f436b929f48b081cb0a99cddd7eb0dcc221064
Larson Laidlaw <larsonlaidlaw@gmail.com>
2020-01-16 01:53:32 -0500

    inital setup, still todos to be removed, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { useQuery, gql } from "@apollo/client";
 
 const Container = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
+
+const GET_USERS = gql`
+  {
+    users {
+      name
+    }
+  }
+`;
+
+interface GetUsersResponse {
+  users: {
+    name: string;
+  };
+}
 
 const App: React.FC = () => {
+  const { loading, error, data } = useQuery<GetUsersResponse>(GET_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <Container>
       <h1>Oscars 2020 SPA</h1>
+      {data && <pre>{JSON.stringify(data.users, null, 2)}</pre>}
     </Container>
   );
-}
+};
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+let apiBase;
+if (window.location.host.match("localhost")) {
+  apiBase = "http://localhost:4000";
+} else {
+  apiBase = "https://oscars-api.alexmarchant.com";
+}
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: apiBase
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
