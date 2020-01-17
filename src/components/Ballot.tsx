21e4984c633f5fc3bfc8f427da138627d5d95dfe
Larson Laidlaw <larsonlaidlaw@gmail.com>
2020-01-17 17:35:14 -0500

    pulling in categories from the api

--- src/components/Ballot.tsx
+++ src/components/Ballot.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+interface Category {
+  id: number
+  title: string
+  value: number
+  nominees: [Nominee]
+}
+
+interface Nominee {
+  id: number
+  name: string
+  film: string
+}
+
+const GET_CATEGORIES = gql`
+  query Categories {
+    categories {
+      id
+      title
+      value
+      nominees {
+        id
+        name
+        film
+      }
+    }
+  }
+`
 
 const Ballot: React.FC = () => {
-  return (
-    <div>
-      <h1>Ballot</h1>
-    </div>
+  const { loading, error, data } = useQuery<{ categories: Category[] }>(
+    GET_CATEGORIES,
   )
+
+  if (loading) return <p>Loading...</p>
+
+  if (error) return <p>{error.message}</p>
+
+  return (
+    <div>{data && <pre>{JSON.stringify(data.categories, null, 2)}</pre>}</div>
+  )
+  // return data && data?.categories.map(category => {
+  //   <div>{category.title}</div>
+  // })}
 }
 
 export default Ballot
