2039cf85d3f23425230160383b7634ed9d1de199
Alex Marchant <alexjmarchant@gmail.com>
2020-01-16 18:50:35 -0600

    remove some cruft and format

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-import { useQuery, gql } from "@apollo/client";
+import React from 'react'
+import styled from 'styled-components'
+import { useQuery, gql } from '@apollo/client'
 
 const Container = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const GET_USERS = gql`
   {
@@ -15,26 +15,26 @@ const GET_USERS = gql`
       name
     }
   }
-`;
+`
 
 interface GetUsersResponse {
   users: {
-    name: string;
-  };
+    name: string
+  }
 }
 
 const App: React.FC = () => {
-  const { loading, error, data } = useQuery<GetUsersResponse>(GET_USERS);
+  const { loading, error, data } = useQuery<GetUsersResponse>(GET_USERS)
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
   return (
     <Container>
       <h1>Oscars 2020 SPA</h1>
       {data && <pre>{JSON.stringify(data.users, null, 2)}</pre>}
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
