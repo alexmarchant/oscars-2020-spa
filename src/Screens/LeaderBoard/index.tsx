544dc06742a47a474723b401b37b781c80051ff3
Alex Marchant <alexjmarchant@gmail.com>
2020-01-26 22:41:02 -0500

    updates

--- src/Screens/LeaderBoard/index.tsx
+++ src/Screens/LeaderBoard/index.tsx
@@ -8,6 +8,21 @@ import {
   CATEGORY_UPDATED,
   CategoryUpdatedRes,
 } from '../../graphql/subscriptions'
+import { Category, Selection } from '../../graphql/shared-types'
+
+function userScore(selections: Selection[], categories: Category[]): number {
+  return categories.reduce((acc: number, category: Category): number => {
+    const selection = selections.find(
+      selection => selection.categoryId === category.id,
+    )
+    if (!selection) return acc
+    if (selection.nomineeId === category.winnerId) {
+      return acc + category.value
+    } else {
+      return acc
+    }
+  }, 0)
+}
 
 const LeaderBoard: React.FC = () => {
   const { loading, error, data } = useQuery<GetCategoriesAndUsersRes>(
@@ -17,13 +32,34 @@ const LeaderBoard: React.FC = () => {
 
   if (loading || !data) return <p>Loading...</p>
   if (error) return <p>{error.message}</p>
-  console.log(GET_CATEGORIES_AND_USERS)
+
+  const userScoreMap: Record<number, number> = data.users.reduce(
+    (acc, user) => {
+      if (!user.selections) {
+        throw new Error('Missing selections')
+      }
+      acc[user.id] = userScore(user.selections, data.categories)
+      return acc
+    },
+    {} as Record<number, number>,
+  )
+  const sortedUsers = data.users.concat().sort((a, b) => {
+    const aScore = userScoreMap[a.id]
+    const bScore = userScoreMap[b.id]
+    if (aScore > bScore) return -1
+    if (aScore < bScore) return 1
+    return 0
+  })
 
   return (
     <ul>
-      {data.users.map(user => (
-        <li>{user.name}</li>
-      ))}
+      {sortedUsers.map(user => {
+        return (
+          <li key={user.id}>
+            {user.name} - {userScoreMap[user.id]}
+          </li>
+        )
+      })}
     </ul>
   )
 }
